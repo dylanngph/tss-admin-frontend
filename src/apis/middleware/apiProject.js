3466fe6309283938572dee6534589089c1a703fd
nguyenz <greenspacevz@gmail.com>
2021-12-03 22:24:56 +0700

    Update code

--- src/apis/middleware/apiProject.js
+++ src/apis/middleware/apiProject.js
@@ -1,7 +1,7 @@
 import { API_PROJECT, API_PROJECT_VERSION } from "apis/config";
 import { callApi } from "./apiRequest";
 
-const callApiAccount = (method, apiURL, args) => {
+const callApiProject = (method, apiURL, args) => {
   const params = {
     method,
     baseURL: API_PROJECT + API_PROJECT_VERSION,
@@ -11,4 +11,4 @@ const callApiAccount = (method, apiURL, args) => {
   return callApi(params);
 };
 
-export default callApiAccount;
+export default callApiProject;
